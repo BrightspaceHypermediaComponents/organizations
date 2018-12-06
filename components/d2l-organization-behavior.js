5fb9caad62a025359c282dd543722bf1a2933938
Noel Stonehouse <noel.stonehouse@d2l.com>
2018-12-06 10:30:18 -0500

    US102461: Update references to Siren Parser and Hypermedia Constants.

--- components/d2l-organization-behavior.js
+++ components/d2l-organization-behavior.js
@@ -1,6 +1,6 @@
 import '@polymer/polymer/polymer-legacy.js';
 import 'd2l-fetch/d2l-fetch.js';
-import 'siren-parser/siren-parser.js';
+import SirenParse from 'siren-parser';
 window.D2L = window.D2L || {};
 window.D2L.PolymerBehaviors = window.D2L.PolymerBehaviors || {};
 window.D2L.PolymerBehaviors.Organization = window.D2L.PolymerBehaviors.Organization || {};
