be2a687c228ff5c5ae78046414db843bbb8334ac
Edward Gao <Edward.Gao@D2L.com>
2019-03-22 12:23:39 -0400

    US105061-componentizing org info

--- components/d2l-organization-info/d2l-organization-info.js
+++ components/d2l-organization-info/d2l-organization-info.js
@@ -5,149 +5,100 @@ Polymer-based web component for a organization info such as course code and seme
 
 @demo demo/d2l-organization-info/d2l-organization-info-demo.html Organization Name
 */
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-import '@polymer/polymer/polymer-legacy.js';
-
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
+import 'd2l-polymer-siren-behaviors/store/entity-behavior.js';
 import { Rels } from 'd2l-hypermedia-constants';
 import 'd2l-icons/d2l-icon.js';
 import 'd2l-icons/tier1-icons.js';
 import '../d2l-organization-behavior.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-const $_documentContainer = document.createElement('template');
+/**
+ * @customElement
+ * @polymer
+ */
+class OrganizationInfo extends mixinBehaviors([
+	D2L.PolymerBehaviors.Siren.EntityBehavior,
+	D2L.PolymerBehaviors.Organization.Behavior
+], PolymerElement) {
+	static get template() {
+		return html`
+			<style>
+				d2l-icon {
+					color: var(--d2l-color-tungsten);
+					--d2l-icon-width: 18px;
+					--d2l-icon-height: 18px;
+				}
+				d2l-icon[hidden] {
+					display: none;
+				}
 
-$_documentContainer.innerHTML = `<dom-module id="d2l-organization-info">
-	<template strip-whitespace="">
-		<style>
-			d2l-icon {
-				color: var(--d2l-color-tungsten);
-				--d2l-icon-width: 18px;
-				--d2l-icon-height: 18px;
+				.uppercase {
+					text-transform: uppercase;
+				}
+
+			</style>
+
+			<span>
+				<span hidden$="[[!showOrganizationCode]]" class="uppercase">[[_organizationCode]]</span>
+				<d2l-icon hidden$="[[!_showSeparator]]" icon="d2l-tier1:bullet"></d2l-icon>
+				<span hidden$="[[!showSemesterName]]"><d2l-organization-name href="[[_semesterHref]]" token="[[token]]"></d2l-organization-name></span>
+			</span>
+		`;
+	}
+	static get properties() {
+		return {
+			showOrganizationCode: {
+				type: Boolean,
+				value: false
+			},
+			showSemesterName: {
+				type: Boolean,
+				value: false
+			},
+
+			_semesterHref: String,
+			_organizationCode: String,
+			_semesterName: String,
+			_showSeparator: {
+				type: Boolean,
+				computed: '_computeShowSeparator(showOrganizationCode, showSemesterName, _organizationCode, _semesterHref)'
 			}
-			d2l-icon[hidden] {
-				display: none;
-			}
+		};
+	}
 
-			.uppercase {
-				text-transform: uppercase;
-			}
+	static get observers() {
+		return [
+			'_loadData(entity)',
+			'_sendVoiceReaderInfo(showOrganizationCode, showSemesterName, _organizationCode, _semesterName)'
+		];
+	}
 
-		</style>
+	static get is() { return 'd2l-organization-info'; }
 
-		<span>
-			<span hidden$="[[!_showOrganizationCode]]" class="uppercase">[[_organizationCode]]</span>
-			<d2l-icon hidden$="[[!_showSeparator]]" icon="d2l-tier1:bullet"></d2l-icon>
-			<span hidden$="[[!_showSemesterName]]">[[_semesterName]]</span>
-		</span>
-	</template>
+	ready() {
+		super.ready();
+		this.addEventListener('d2l-organization-accessible', this._onD2lOrganizationAccessible.bind(this));
+	}
 
-	
-</dom-module>`;
+	_loadData(entity) {
+		this._organizationCode = entity && entity.properties && entity.properties.code;
+		this._semesterHref = entity && entity.hasLinkByRel(Rels.parentSemester) && entity.getLinkByRel(Rels.parentSemester).href;
+	}
 
-document.head.appendChild($_documentContainer.content);
-Polymer({
-	is: 'd2l-organization-info',
-
-	properties: {
-		href: String,
-		presentationHref: String,
-
-		_organization: Object,
-		_organizationCode: String,
-		_semesterName: String,
-		_showOrganizationCode: {
-			type: Boolean,
-			value: false
-		},
-		_showSemesterName: {
-			type: Boolean,
-			value: false
-		},
-		_showSeparator: {
-			type: Boolean,
-			value: false,
-			computed: '_computeShowSeparator(_showOrganizationCode, _showSemesterName, _organizationCode, _semesterName)'
-		}
-	},
-
-	behaviors: [
-		D2L.PolymerBehaviors.Organization.Behavior
-	],
-
-	observers: [
-		'_fetchOrganization(href)',
-		'_fetchPresentation(presentationHref)',
-		'_fetchSemester(_organization, _showSemesterName)',
-		'_sendVoiceReaderInfo(_showOrganizationCode, _showSemesterName, _organizationCode, _semesterName)'
-	],
-	_computeShowSeparator: function(showOrganizationCode, showSemester, organizationCode, semesterName) {
+	_computeShowSeparator(showOrganizationCode, showSemester, organizationCode, semesterHref) {
 		return showSemester
 			&& showOrganizationCode
-			&& semesterName
-			&& semesterName.length > 0
 			&& organizationCode
-			&& organizationCode.length > 0;
-	},
+			&& semesterHref
+	}
 
-	_fetchOrganization: function(organizationHref) {
-		this._semesterName = null;
-		this._organizationCode = null;
-		return this._fetchSirenEntity(organizationHref)
-			.then(function(organizationEntity) {
-				this._statusTextEnded = null;
-				this._statusTextFuture = null;
-				this._statusTextInactive = null;
-
-				if (!organizationEntity) {
-					return;
-				}
-				this._organization = organizationEntity;
-
-				if (!organizationEntity.properties) {
-					return;
-				}
-
-				this._organizationCode = organizationEntity.properties.code;
-			}.bind(this));
-	},
-
-	_fetchPresentation: function(presentationHref) {
-		if (!presentationHref) {
-			return Promise.resolve();
+	_onD2lOrganizationAccessible(e) {
+		if (e.detail.organization.name) {
+			this._semesterName = e.detail.organization.name;
 		}
+	}
 
-		return this._fetchSirenEntity(presentationHref)
-			.then(function(presentationEntity) {
-				this._showOrganizationCode = presentationEntity
-					&& presentationEntity.properties
-					&& presentationEntity.properties.ShowCourseCode;
-				this._showSemesterName = presentationEntity
-					&& presentationEntity.properties
-					&& presentationEntity.properties.ShowSemester;
-			}.bind(this));
-	},
-
-	_fetchSemester: function(organization, showSemesterName) {
-		if (!showSemesterName
-			|| !organization
-			|| !organization.hasLinkByRel(Rels.parentSemester)
-		) {
-			return Promise.resolve();
-		}
-
-		var semesterHref = organization.getLinkByRel(Rels.parentSemester).href;
-		return this._fetchSirenEntity(semesterHref)
-			.then(function(semesterEntity) {
-				this._semesterName = semesterEntity
-					&& semesterEntity.properties
-					&& semesterEntity.properties.name;
-			}.bind(this));
-	},
-
-	_sendVoiceReaderInfo: function(showOrganizationCode, showSemesterName, organizationCode, semesterName) {
+	_sendVoiceReaderInfo(showOrganizationCode, showSemesterName, organizationCode, semesterName) {
 		var details = {
 			organization: {
 				code: showOrganizationCode && organizationCode
@@ -156,5 +107,7 @@ Polymer({
 		};
 
 		this._fireD2lOrganizationAccessible(details);
-	},
-});
+	}
+}
+
+window.customElements.define(OrganizationInfo.is, OrganizationInfo);
