4cb813451993794cc1ed6a5e3a543095a9658320
Edward Gao <Edward.Gao@D2L.com>
2019-03-27 16:38:34 -0400

    US104997-remove siren parse import

--- components/d2l-organization-behavior.js
+++ components/d2l-organization-behavior.js
@@ -1,6 +1,5 @@
 import '@polymer/polymer/polymer-legacy.js';
 import 'd2l-fetch/d2l-fetch.js';
-import SirenParse from 'siren-parser';
 window.D2L = window.D2L || {};
 window.D2L.PolymerBehaviors = window.D2L.PolymerBehaviors || {};
 window.D2L.PolymerBehaviors.Organization = window.D2L.PolymerBehaviors.Organization || {};
