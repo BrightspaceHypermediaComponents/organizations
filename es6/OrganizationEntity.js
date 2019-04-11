54786722c43dbe3d40c63c7522db80a01f83a428
Maya Jones <Maya.Jones@D2L.com>
2019-04-10 22:38:52 -0400

    test

--- es6/OrganizationEntity.js
+++ es6/OrganizationEntity.js
@@ -1,8 +1,29 @@
 'use strict';
 
-import { Entity } from 'siren-sdk/es6/Entity.js';
 import { Rels } from 'd2l-hypermedia-constants';
+class Entity {
+	/**
+	 * Primes the object used by the entityFactory. Should never be called outside.
+	 * @param {Object} entity A hypermedia siren entity as defined by [the siren specification]{@link https://github.com/kevinswiber/siren}
+	 * @param {String|Function|null} token JWT Token for brightspace | a function that returns a JWT token for brightspace | null (defaults to cookie authentication in a browser)
+	 * @param {Function} listener Listener helper class
+	 */
+	constructor(entity, token, listener) {
+		if (new.target === Entity) {
+			throw new TypeError('Cannot construct Entity instances directly');
+		}
+		this._entity = entity;
+		this._token = token;
+		this._listener = listener;
+	}
 
+	/**
+	 * Cleans up this entity by deleting the callbacks listeners stored in the entity store.
+	 */
+	dispose() {
+		this._listener.remove();
+	}
+}
 export class OrganizationEntity extends Entity {
 	// Entity has a constructor that is called from the factory to keep track of what is required to be cleaned.
 	name() {
