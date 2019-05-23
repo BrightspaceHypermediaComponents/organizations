2b88fc8371b4c4adc873ff4102bc6fccdf3e28fd
Chester Twomey <chester.twomey@d2l.com>
2019-05-23 09:21:29 -0500

    Move OrganizationEntity to siren sdk (#68)
    * update references
    
    * increment siren sdk
    
    * fix references

--- components/d2l-organization-info/d2l-organization-info.js
+++ components/d2l-organization-info/d2l-organization-info.js
@@ -6,8 +6,8 @@ Polymer-based web component for a organization info such as course code and seme
 @demo demo/d2l-organization-info/d2l-organization-info-demo.html Organization Name
 */
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
-import { EntityMixin } from 'siren-sdk/mixin/entity-mixin.js';
-import { OrganizationEntity } from '../../OrganizationEntity.js';
+import { EntityMixin } from 'siren-sdk/src/mixin/entity-mixin.js';
+import { OrganizationEntity } from 'siren-sdk/src/organizations/OrganizationEntity.js';
 import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
 import 'd2l-icons/d2l-icon.js';
 import 'd2l-icons/tier1-icons.js';
