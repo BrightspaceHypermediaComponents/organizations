a7eb9aa577235e4b8b14567aa919f2cf01cdc38f
Chester Twomey <Chester.Twomey@D2L.com>
2019-06-04 14:52:36 -0500

    basic demo for newer consortium tabs

--- components/d2l-organization-consortium/d2l-organization-consortium-tabs.js
+++ components/d2l-organization-consortium/d2l-organization-consortium-tabs.js
@@ -6,34 +6,26 @@ Polymer-based web component for displaying all organizations a user is enrolled 
 @demo demo/d2l-organization-consortium/d2l-organization-consortium.html organization consortium tab
 */
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
-import { EntityMixin } from 'siren-sdk/mixin/entity-mixin.js';
-import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
+import { EntityMixin } from 'siren-sdk/src/mixin/entity-mixin.js';
 
-import 'd2l-tabs/d2l-tabs.js';
-import 'd2l-tabs/d2l-tab-panel.js';
-
-import  { ConsortiumEntityMock } from './consortium-entity-mock.js'; //todo move to siren-sdk
+import { ConsortiumTokenCollectionEntity } from 'siren-sdk/src/consortium/ConsortiumTokenCollectionEntity.js';
 import '../d2l-organization-behavior.js';
 
 /**
  * @customElement
  * @polymer
  */
-class OrganizationConsortiumTabs extends mixinBehaviors([
-	D2L.PolymerBehaviors.Organization.Behavior
-], EntityMixin(PolymerElement)) {
+class OrganizationConsortiumTabs extends EntityMixin(PolymerElement) {
 	constructor() {
 		super();
-		this._setEntityType(ConsortiumEntityMock);
+		this._setEntityType(ConsortiumTokenCollectionEntity);
 	}
 
 	static get template() {
 		return html`
-		<d2l-tabs>
-			<template items="[[organizations]]" is="dom-repeat">
-				<d2l-tab-panel text="[[item.name]]" selected$="[[_isSelected(item)]]" on-d2l-tab-panel-selected="_navigate"></d2l-tab-panel>
-			</template>
-		</d2l-tabs>
+		<template items="[[parsedOrganizations]]" is="dom-repeat">
+			<a href="[[item.value]]">[[item.name]]</a>
+		</template>
 		`;
 	}
 	_navigate(e) {
@@ -48,36 +40,44 @@ class OrganizationConsortiumTabs extends mixinBehaviors([
 		return windowHost === orgHost;
 	}
 
-	static get properties() {
-		return {
-			organizations: {
-				type: Array,
-				value: [
-					{
-						name: 'D2L',
-						organizationHomepageUrl: 'https://www.d2l.com'
-					},
-					{
-						name: 'Brightspace Community',
-						organizationHomepageUrl: 'https://community.brightspace.com/s/'
-
-					},
-					{
-						name: 'demo page',
-						organizationHomepageUrl: 'http://localhost:8081/components/d2l-organizations/demo/d2l-organization-consortium/d2l-organization-consortium.html'
-					},
-					{
-						name: 'other demo page',
-						organizationHomepageUrl: 'http://127.0.0.1:8081/components/d2l-organizations/demo/d2l-organization-consortium/d2l-organization-consortium.html'
-					}
-				]
-			}
-		};
+	_onConsortiumChange(consotriumTokenCollection) {
+		consotriumTokenCollection.consortiumTokenEntities((consortiumEntity) => {
+			consortiumEntity.rootOrganizationEntity((rootEntity) => {
+				rootEntity.organization((orgEntity) => {
+					this.set(`organizations.${orgEntity.code() || orgEntity.name()}`, orgEntity.fullyQualifiedOrganizationHomepageUrl());
+				});
+			});
+		});
 	}
 
 	static get observers() {
 		return [
+			'_onConsortiumChange(_entity)'
 		];
+	}
+
+	_computeParsedOrganizations() {
+		const currentOrganizations = this.organizations;
+		return Object.keys(currentOrganizations).map(function(key) {
+			return {
+				name: key,
+				value: currentOrganizations[key]
+			};
+		});
+	}
+
+	static get properties() {
+		return {
+			organizations: {
+				type: Object,
+				value: {},
+				notify: true
+			},
+			parsedOrganizations: {
+				type: Array,
+				computed: '_computeParsedOrganizations(organizations.*)'
+			}
+		};
 	}
 
 	static get is() { return 'd2l-organization-consortium-tabs'; }
