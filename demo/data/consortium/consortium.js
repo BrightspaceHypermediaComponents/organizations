3450575eac7e52c675cec0b3ae5eaa668188bbf9
Chester Twomey <chester.twomey@d2l.com>
2019-07-16 12:06:38 -0500

    fix demo page for consortiums (#95)
    * fix demo page for consortiums
    
    * format
    
    * lint

---
+++ demo/data/consortium/consortium.js
@@ -0,0 +1,50 @@
+export const consortium = {
+	'class': ['tokens'],
+	'entities': [
+		{
+			'class': [
+				'token'
+			],
+			'rel': [],
+			'properties': {
+				'tenant': '1cb16d6a-8557-4850-8846-3fa9b6174494',
+				'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwidGVuYW50aWQiOiIxY2IxNmQ2YS04NTU3LTQ4NTAtODg0Ni0zZmE5YjYxNzQ0OTQiLCJpYXQiOjE1MTYyMzkwMjJ9.z5hMu02Cx8p7Lw0a_1nBVTkMD2UncN5UZre3l0SWo7c'
+			},
+			'links': [
+				{
+					'rel': ['self'],
+					'href': 'https://consortium.api.dev.brightspace.com/1cb16d6a-8557-4850-8846-3fa9b6174494'
+				},
+				{
+					'rel': ['https://api.brightspace.com/rels/root'],
+					'href': '../data/consortium/root1-consortium.json'
+				}
+			]
+		},
+		{
+			'class': [
+				'token'
+			],
+			'rel': [],
+			'properties': {
+				'tenant': '8b33e567-c616-4667-868b-fdfe9edc3a78',
+				'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwidGVuYW50aWQiOiI4YjMzZTU2Ny1jNjE2LTQ2NjctODY4Yi1mZGZlOWVkYzNhNzgiLCJpYXQiOjE1MTYyMzkwMjJ9.cQjR28qT_c-os_FeFy4-L1NhCIY-9utPLzSHrzIMuOc'
+			},
+			'links': [
+				{
+					'rel': ['self'],
+					'href': 'https://consortium.api.dev.brightspace.com/8b33e567-c616-4667-868b-fdfe9edc3a78'
+				},
+				{
+					'rel': ['https://api.brightspace.com/rels/root'],
+					'href': '../data/consortium/root2-consortium.json'
+				}
+			]
+		}
+	],
+	'links': [{
+		'rel': ['self'],
+		'href': '/consortium.json'
+	}
+	]
+};
