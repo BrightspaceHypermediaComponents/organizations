7caec150fce51d74b72a0ec68c9c8682d9cf554c
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2019-10-21 11:34:25 -0400

    Remove unneeded specificity and reorganize css to group together in order of element flow, use background-color and rearrange border ordering

--- components/d2l-organization-consortium/d2l-organization-consortium-tabs.js
+++ components/d2l-organization-consortium/d2l-organization-consortium-tabs.js
@@ -92,9 +92,72 @@ class OrganizationConsortiumTabs extends EntityMixin(OrganizationConsortiumLocal
 		return html`
 		<style include="d2l-typography-shared-styles">
 
+			.d2l-consortium-tab-box {
+				display: flex;
+				flex-wrap: nowrap;
+				max-height: 0;
+				overflow-x: unset;
+				overflow-y: hidden;
+			}
+			.d2l-consortium-tab-growIn {
+				max-height: 1.5rem;
+				transform-origin: top;
+				transition: max-height 500ms ease-in;
+			}
+			.d2l-consortium-tab-showTabs {
+				max-height: 1.5rem;
+			}
+
+			.d2l-tab-container {
+				border: 1px solid rgba(255, 255, 255, .30);
+				border-radius: 6px 6px 0 0;
+				border-bottom: none;
+				display: inline-block;
+				margin: 4px 0 0 -1px;
+				position: relative;
+			}
+			:host(:dir(rtl)) .d2l-tab-container {
+				margin-left: 0;
+				margin-right: -1px;
+			}
+			.d2l-tab-container:first-child {
+				margin-left: 0;
+				margin-right: 0;
+			}
+			.d2l-tab-container:hover {
+				border-color: rgba(255, 255, 255, .60);
+				border-bottom: none;
+			}
+			.d2l-tab-container[selected] {
+				border-color: rgba(0, 0, 0, .42);
+				border-bottom: none;
+				z-index: 1;
+			}
+
+			.d2l-consortium-tab {
+				background-color: rgba(0, 0, 0, .54);
+				border-bottom: none;
+				border-radius: 5px 5px 0 0;
+				display: block;
+				max-width: 5.5rem;
+				outline: none;
+				padding: 0 0.6rem;
+			}
+			.d2l-consortium-tab:hover {
+				background-color: rgba(0, 0, 0, .70);
+			}
+			.d2l-consortium-tab:focus {
+				box-shadow: inset 0 0 0 2px rgba(0, 0, 0, .54),
+							inset 0 0 0 2px var(--d2l-branding-primary-color, var(--d2l-color-celestine)),
+							inset 0 0 0 4px #ffffff;
+			}
+			[selected] .d2l-consortium-tab {
+				background-color: white;
+			}
+
 			.d2l-consortium-tab-content {
 				@apply --d2l-body-small-text;
-				color: white;
+				color: #ffffff;
 				cursor: pointer;
 				display: inline-block;
 				line-height: 1.25rem;
@@ -106,36 +169,7 @@ class OrganizationConsortiumTabs extends EntityMixin(OrganizationConsortiumLocal
 				word-break: break-all;
 				vertical-align: middle;
 			}
-			.d2l-consortium-tab-content d2l-icon {
-				--d2l-icon-fill-color: white;
-				padding-right: 6px;
-				vertical-align: middle;
-			}
-			:host(:dir(rtl)) .d2l-consortium-tab-content d2l-icon {
-				padding-left: 6px;
-				padding-right: 0;
-			}
-			.d2l-consortium-tab {
-				background: rgba(0, 0, 0, .54);
-				border-bottom: none;
-				border-radius: 5px 5px 0 0;
-				display: block;
-				max-width: 5.5rem;
-				outline: none;
-				padding: 0 0.6rem;
-			}
-			.d2l-consortium-tab:hover {
-				background: rgba(0, 0, 0, .70);
-			}
-			.d2l-tab-container .d2l-consortium-tab:focus {
-				box-shadow: inset 0 0 0 2px rgba(0, 0, 0, .54),
-							inset 0 0 0 2px var(--d2l-branding-primary-color, var(--d2l-color-celestine)),
-							inset 0 0 0 4px white;
-			}
-			[selected] .d2l-consortium-tab {
-				background: white;
-			}
-			[selected] .d2l-consortium-tab > .d2l-consortium-tab-content {
+			[selected] .d2l-consortium-tab-content {
 				color: var(--d2l-color-ferrite);
 				cursor: default;
 			}
@@ -143,49 +177,16 @@ class OrganizationConsortiumTabs extends EntityMixin(OrganizationConsortiumLocal
 			.d2l-consortium-tab-content.d2l-consortium-tab-loading {
 				cursor: default;
 			}
-			.d2l-consortium-tab-box {
-				display: flex;
-				flex-wrap: nowrap;
-				max-height: 0;
-				overflow-x: unset;
-				overflow-y: hidden;
+			.d2l-consortium-tab-content d2l-icon {
+				--d2l-icon-fill-color: #ffffff;
+				padding-right: 6px;
+				vertical-align: middle;
 			}
-			.d2l-consortium-tab-box .d2l-tab-container {
-				margin-left: -1px;
+			:host(:dir(rtl)) .d2l-consortium-tab-content d2l-icon {
+				padding-left: 6px;
+				padding-right: 0;
 			}
-			:host(:dir(rtl)) .d2l-consortium-tab-box .d2l-tab-container {
-				margin-left: 0;
-				margin-right: -1px;
-			}
-			.d2l-consortium-tab-box .d2l-tab-container:first-child {
-				margin-left: 0;
-				margin-right: 0;
-			}
-			.d2l-tab-container {
-				border: rgba(255, 255, 255, .30) solid 1px;
-				border-radius: 6px 6px 0 0;
-				border-bottom: none;
-				display: inline-block;
-				margin: 4px 0 0 0;
-				position: relative;
-			}
-			.d2l-tab-container:hover {
-				border: rgba(255, 255, 255, .60) solid 1px;
-				border-bottom: none;
-			}
-			.d2l-tab-container[selected] {
-				border: rgba(0, 0, 0, .42) solid 1px;
-				border-bottom: none;
-				z-index: 1;
-			}
-			.d2l-consortium-tab-growIn {
-				max-height: 1.5rem;
-				transform-origin: top;
-				transition: max-height 500ms ease-in;
-			}
-			.d2l-consortium-tab-showTabs {
-				max-height: 1.5rem;
-			}
+
 			d2l-navigation-notification-icon {
 				pointer-events: none;
 				right: -1px;
