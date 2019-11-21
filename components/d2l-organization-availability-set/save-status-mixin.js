83ce19d3f9200ee4b119a1124d1efc348c28dcf4
Vincent Wong <Vincent.Wong@D2L.com>
2019-11-21 11:40:34 -0800

    Remove organization-availability-set specific event

--- components/d2l-organization-availability-set/save-status-mixin.js
+++ components/d2l-organization-availability-set/save-status-mixin.js
@@ -5,19 +5,9 @@ export const SaveStatusMixin = superclass => class extends superclass {
 			composed: true
 		}));
 
-		this.dispatchEvent(new CustomEvent('d2l-organization-availability-set-save-start', {
-			bubbles: true,
-			composed: true
-		}));
-
 		return promise
 			.then(result => {
 				this.dispatchEvent(new CustomEvent('d2l-siren-entity-save-end', {
-					bubbles: true,
-					composed: true
-				}));
-
-				this.dispatchEvent(new CustomEvent('d2l-organization-availability-set-save', {
 					bubbles: true,
 					composed: true
 				}));
@@ -26,12 +16,6 @@ export const SaveStatusMixin = superclass => class extends superclass {
 			})
 			.catch(error => {
 				this.dispatchEvent(new CustomEvent('d2l-siren-entity-save-error', {
-					error,
-					bubbles: true,
-					composed: true
-				}));
-
-				this.dispatchEvent(new CustomEvent('d2l-organization-availability-set-save-error', {
 					error,
 					bubbles: true,
 					composed: true
