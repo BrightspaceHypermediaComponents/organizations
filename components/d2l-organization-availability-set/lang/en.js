eef69fb33e5faef40b64ba68ee4a685a0cd5a0d1
Vincent Wong <Vincent.Wong@D2L.com>
2019-11-25 13:01:34 -0800

    Add context for the translation

--- components/d2l-organization-availability-set/lang/en.js
+++ components/d2l-organization-availability-set/lang/en.js
@@ -2,10 +2,10 @@
 
 export default {
 	"addOrgUnits": "Add Org Units", // Add Org Units button
-	"currentOrgUnitItemDescription": "Current Org Unit: {name}", // Description of the current orgunit availability item
-	"explicitItemDescription": "The {type}: {name}", // Description of an explicit orgunit availability item
-	"inheritItemDescription": "Every Org Unit under the {type}: {name}", // Description of an inherit (all descendants) orgunit availability item
-	"inheritItemWithDescendantTypeDescription": "Every {descendantType} under the {type}: {name}", // Description of an inherit with specific {descendantType} orgunit availability item
+	"currentOrgUnitItemDescription": "Current Org Unit: {name}", // Label for a current orgunit availability
+	"explicitItemDescription": "The {type}: {name}", // Label for an explicit orgunit availability with org unit type and name
+	"inheritItemDescription": "Every Org Unit under the {type}: {name}", // Label for an inherit (all descendants) orgunit availability
+	"inheritItemWithDescendantTypeDescription": "Every {descendantType} under the {type}: {name}", // Label for an inherit with specific {descendantType} orgunit availability
 	"removeAvailabilityFor": "Remove availability for {itemDescription}", // Alt text for remove availability 'X' button
-	"availabilityRemoved": "Availability removed for {itemDescription}"
+	"availabilityRemoved": "Availability removed for {itemDescription}" // Aria announcement when an availability is removed
 };
