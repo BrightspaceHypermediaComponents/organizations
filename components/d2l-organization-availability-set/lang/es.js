10c1ffb8ee7a128c2f67f692d7036ba5c0c99c96
JenkinsReader <resupport@desire2learn.com>
2019-11-27 13:13:15 -0500

    Translation Updates

---
+++ components/d2l-organization-availability-set/lang/es.js
@@ -0,0 +1,11 @@
+/* eslint quotes: 0 */
+
+export default {
+	"addOrgUnits": "Add Org Units", // Add Org Units button
+	"currentOrgUnitItemDescription": "Current Org Unit: {name}", // Label for a current orgunit availability
+	"explicitItemDescription": "The {type}: {name}", // Label for an explicit orgunit availability with org unit type and name
+	"inheritItemDescription": "Every Org Unit under the {type}: {name}", // Label for an inherit (all descendants) orgunit availability
+	"inheritItemWithDescendantTypeDescription": "Every {descendantType} under the {type}: {name}", // Label for an inherit with specific {descendantType} orgunit availability
+	"removeAvailabilityFor": "Remove availability for {itemDescription}", // Alt text for remove availability 'X' button
+	"availabilityRemoved": "Availability removed for {itemDescription}" // Aria announcement when an availability is removed
+};
