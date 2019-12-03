4d02043fa6704622bd295fbf840a0fb25e478e03
Vincent Wong <Vincent.Wong@D2L.com>
2019-12-02 17:58:44 -0800

    US108212  Check/Uncheck current org unit

--- components/d2l-organization-availability-set/d2l-current-organization-availability.js
+++ components/d2l-organization-availability-set/d2l-current-organization-availability.js
@@ -1,5 +1,6 @@
 import 'd2l-inputs/d2l-input-checkbox';
 import { css, html, LitElement } from 'lit-element/lit-element';
+import { announce } from '@brightspace-ui/core/helpers/announce.js';
 import { EntityMixinLit } from 'siren-sdk/src/mixin/entity-mixin-lit.js';
 import { getLocalizeResources } from './localization.js';
 import { LocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin.js';
@@ -54,13 +55,39 @@ class CurrentOrganizationAvailability extends EntityMixinLit(LocalizeMixin(LitEl
 		}
 	}
 
+	_delete(e) {
+		const checkboxElem = e.target;
+		const promise = () => {
+			return super._entity.delete().then(() => {
+				announce(this.localize('notAvailableToCurrentOrgUnit', { name: this._name }));
+			}).catch((error) => {
+				checkboxElem.checked = true;
+				return Promise.reject(error);
+			});
+		};
+
+		this.dispatchEvent(
+			new CustomEvent('delete-organization-availability', {
+				bubbles: true,
+				composed: true,
+				detail: { promise }
+			})
+		);
+	}
+
+	_onCheckboxChange(e) {
+		if (!e.target.checked) {
+			this._delete(e);
+		}
+	}
+
 	get _itemDescription() {
 		return this.localize('currentOrgUnitItemDescription', { name: this._name });
 	}
 
 	render() {
 		return html`
-			<d2l-input-checkbox checked ?disabled="${!this._canDelete}">
+			<d2l-input-checkbox checked ?disabled="${!this._canDelete}" @change="${this._onCheckboxChange}">
 				${this._itemDescription}
 			</d2l-input-checkbox>
 		`;
