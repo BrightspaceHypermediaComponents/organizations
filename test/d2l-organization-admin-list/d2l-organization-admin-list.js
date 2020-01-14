b416abd573d6911b780f36add658f0a5ebb7182c
Maya Jones <Maya.Jones@D2L.com>
2020-01-14 15:39:26 -0500

    Fix test for reals

--- test/d2l-organization-admin-list/d2l-organization-admin-list.js
+++ test/d2l-organization-admin-list/d2l-organization-admin-list.js
@@ -8,23 +8,25 @@ describe('d2l-organization-admin-list', () => {
 		element = fixture('admin-list');
 		await element.updateComplete;
 
-		collectionEntity = {
-			subEntitiesLoaded: () => Promise.resolve()
-		};
+		collectionEntity = {};
 	});
 
 	it('should pass all axe tests', async() => {
 		await runAxe(element);
 	});
 
-	it('should reset items on collection changed', () => {
+	it('should reset items on collection changed', done => {
 		collectionEntity.onOrganizationsChange = () => {};
 		collectionEntity.totalPages = () => 1;
 		collectionEntity.currentPage = () => 1;
+		collectionEntity.subEntitiesLoaded = () => Promise.resolve();
 		element._items = ['non', 'empty', 'items', 'array'];
 
 		element._onOrganizationCollectionChanged(collectionEntity);
 
-		expect(element._items).to.be.empty;
+		collectionEntity.subEntitiesLoaded().then(() => {
+			expect(element._items).to.be.empty;
+			done();
+		});
 	});
 });
