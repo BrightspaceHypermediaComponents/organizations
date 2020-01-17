b82339517504cbec33bdf64c826c5d603b6aed62
Maya Jones <Maya.Jones@D2L.com>
2020-01-17 16:18:13 -0500

    Oops tabs instead of spaves

--- components/d2l-organization-admin-list/lang/en.js
+++ components/d2l-organization-admin-list/lang/en.js
@@ -1,18 +1,18 @@
 /* eslint quotes: 0 */
 
 export default {
-    "confirmDeleteMessage": "This item will be moved to the recycle bin and can be restored or permanently deleted later.", // The message for the delete Learning Path confirmation dialog
-    "confirmDeleteTitle": "Confirm Delete", // The title for the delete Learning Path confirmation dialog
-    "createLearningPath": "Create Learning Path", // Button to create a new Learning Path
-    "defaultLearningPathName": "Untitled Learning Path", // Default name given to a newly created Learning Path
-    "deleteSucceeded": "Deleted Successfully", // The toast message displayed when a Learning Path is successfully deleted
-    "noAction": "Cancel", // The 'No' button action text in the delete Learning Path confirmation dialog
-    "noLearningPath": "There are no learning paths to display", // A page that lists learning paths this shows up when there are none to list.
-    "noLearningPathWithSearchTerm": "0 search results found for \"{searchTerm}\"", // After searching for learning paths this shows up when there are no search results.
-    "pageNext": "To next page", // Label for the next page button
-    "pagePrevious": "To previous page", // Label for the back to previous page button
-    "pageSelection": "On page {pageCurrent} of {pageTotal}. Enter a page number to go to that page", // Label for the page number input that lists the current page and lets the user jump to specific pages
-    "search": "Search", // Label for the search input to search the list of org units
-    "searchPlaceholder": "Search...", // Placeholder text for the search input to search the list of org units
-    "yesAction": "Delete Learning Path" // The 'Yes' button action text in the delete Learning Path confirmation dialog
+	"confirmDeleteMessage": "This item will be moved to the recycle bin and can be restored or permanently deleted later.", // The message for the delete Learning Path confirmation dialog
+	"confirmDeleteTitle": "Confirm Delete", // The title for the delete Learning Path confirmation dialog
+	"createLearningPath": "Create Learning Path", // Button to create a new Learning Path
+	"defaultLearningPathName": "Untitled Learning Path", // Default name given to a newly created Learning Path
+	"deleteSucceeded": "Deleted Successfully", // The toast message displayed when a Learning Path is successfully deleted
+	"noAction": "Cancel", // The 'No' button action text in the delete Learning Path confirmation dialog
+	"noLearningPath": "There are no learning paths to display", // A page that lists learning paths this shows up when there are none to list.
+	"noLearningPathWithSearchTerm": "0 search results found for \"{searchTerm}\"", // After searching for learning paths this shows up when there are no search results.
+	"pageNext": "To next page", // Label for the next page button
+	"pagePrevious": "To previous page", // Label for the back to previous page button
+	"pageSelection": "On page {pageCurrent} of {pageTotal}. Enter a page number to go to that page", // Label for the page number input that lists the current page and lets the user jump to specific pages
+	"search": "Search", // Label for the search input to search the list of org units
+	"searchPlaceholder": "Search...", // Placeholder text for the search input to search the list of org units
+	"yesAction": "Delete Learning Path" // The 'Yes' button action text in the delete Learning Path confirmation dialog
 };
