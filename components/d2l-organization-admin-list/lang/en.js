7fbc2c5b243ee74495799f7f03a72c7c52af19cb
Maya Jones <Maya.Jones@D2L.com>
2020-01-17 16:17:13 -0500

    search results with no caps

--- components/d2l-organization-admin-list/lang/en.js
+++ components/d2l-organization-admin-list/lang/en.js
@@ -8,7 +8,7 @@ export default {
     "deleteSucceeded": "Deleted Successfully", // The toast message displayed when a Learning Path is successfully deleted
     "noAction": "Cancel", // The 'No' button action text in the delete Learning Path confirmation dialog
     "noLearningPath": "There are no learning paths to display", // A page that lists learning paths this shows up when there are none to list.
-    "noLearningPathWithSearchTerm": "0 Search Results found for \"{searchTerm}\"", // After searching for learning paths this shows up when there are no search results.
+    "noLearningPathWithSearchTerm": "0 search results found for \"{searchTerm}\"", // After searching for learning paths this shows up when there are no search results.
     "pageNext": "To next page", // Label for the next page button
     "pagePrevious": "To previous page", // Label for the back to previous page button
     "pageSelection": "On page {pageCurrent} of {pageTotal}. Enter a page number to go to that page", // Label for the page number input that lists the current page and lets the user jump to specific pages
