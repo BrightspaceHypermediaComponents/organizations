34456f0299748b086e860a06925a5c8b3bb9b204
Zina Ramirez <instinctualart@gmail.com>
2020-08-21 14:51:50 -0400

    Converted admin-list tests

---
+++ components/d2l-organization-admin-list/test/d2l-organization-admin-list.test.js
@@ -0,0 +1,43 @@
+import '../d2l-organization-admin-list.js';
+import { expect, fixture, html } from '@open-wc/testing';
+import { runConstructor } from '@brightspace-ui/core/tools/constructor-test-helper.js';
+
+const basic = html`<d2l-organization-admin-list titleText="Learning Paths"></d2l-organization-admin-list>`;
+
+describe('d2l-organization-admin-list', () => {
+	let el;
+	let collectionEntity;
+
+	describe('accessibility', () => {
+		it('should pass all axe tests', async() => {
+			el = await fixture(basic);
+			await expect(el).to.be.accessible();
+		});
+	});
+
+	describe('constructor', () => {
+		it('should construct', () => {
+			runConstructor('d2l-organization-admin-list');
+		});
+	});
+
+	beforeEach(async() => {
+		el = await fixture(basic);
+		collectionEntity = {};
+	});
+
+	it('should reset items on collection changed', done => {
+		collectionEntity.onOrganizationsChange = () => {};
+		collectionEntity.totalPages = () => 1;
+		collectionEntity.currentPage = () => 1;
+		collectionEntity.subEntitiesLoaded = () => Promise.resolve();
+		el._items = ['non', 'empty', 'items', 'array'];
+
+		el._onOrganizationCollectionChanged(collectionEntity);
+
+		collectionEntity.subEntitiesLoaded().then(() => {
+			expect(el._items).to.be.empty;
+			done();
+		});
+	});
+});
