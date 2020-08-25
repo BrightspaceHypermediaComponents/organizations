501d39fa2a3f4493a44f9434b096833b1659f444
JenkinsReader <resupport@desire2learn.com>
2020-08-25 13:29:02 -0400

    Translation Updates

---
+++ components/d2l-organization-availability-set/lang/es-es.js
@@ -0,0 +1,13 @@
+/* eslint quotes: 0 */
+
+export default {
+	"addOrgUnits": "Agregar unidades de organización", // Add Org Units button
+	"currentOrgUnitItemDescription": "Unidad de organización actual: {name}", // Label for a current orgunit availability
+	"explicitItemDescription": "El {type}: {name}", // Label for an explicit orgunit availability with org unit type and name
+	"inheritItemDescription": "Todas las unidades de organización en {type}: {name}", // Label for an inherit (all descendants) orgunit availability
+	"inheritItemWithDescendantTypeDescription": "Cada {descendantType} en {type}: {name}", // Label for an inherit with specific {descendantType} orgunit availability
+	"removeAvailabilityFor": "Quitar disponibilidad para {itemDescription}", // Alt text for remove availability 'X' button
+	"availabilityRemoved": "Disponibilidad quitada para {itemDescription}", // Aria announcement when an availability is removed
+	"notAvailableToCurrentOrgUnit": "No disponible para la unidad de organización actual: {name}", // Aria announcement. {name} is current org unit name.
+	"availableToCurrentOrgUnit": "Disponible para la unidad de organización actual: {name}", // Aria announcement. {name} is current org unit name.
+};
---
+++ components/d2l-organization-availability-set/lang/fr-fr.js
@@ -0,0 +1,13 @@
+/* eslint quotes: 0 */
+
+export default {
+	"addOrgUnits": "Ajouter des unités organisationnelles", // Add Org Units button
+	"currentOrgUnitItemDescription": "Unité organisationnelle actuelle : {name}", // Label for a current orgunit availability
+	"explicitItemDescription": "Type {type} : {name}", // Label for an explicit orgunit availability with org unit type and name
+	"inheritItemDescription": "Chaque unité organisationnelle sous le type {type} : {name}", // Label for an inherit (all descendants) orgunit availability
+	"inheritItemWithDescendantTypeDescription": "Chaque {descendentType} sous le type {type} : {name}", // Label for an inherit with specific {descendantType} orgunit availability
+	"removeAvailabilityFor": "Supprimer la disponibilité pour {itemDescription}", // Alt text for remove availability 'X' button
+	"availabilityRemoved": "Disponibilité supprimée pour {itemDescription}", // Aria announcement when an availability is removed
+	"notAvailableToCurrentOrgUnit": "Non disponible pour l’unité organisationnelle actuelle : {name}", // Aria announcement. {name} is current org unit name.
+	"availableToCurrentOrgUnit": "Disponible pour l’unité organisationnelle actuelle : {name}", // Aria announcement. {name} is current org unit name.
+};
