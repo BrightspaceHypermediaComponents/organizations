40c14da0fb35e5e9eff1016140b9116f795cb0fd
Zina Ramirez <instinctualart@gmail.com>
2020-08-25 17:37:39 -0400

    Apply suggestions from code review
    Fixing lint issues

--- components/d2l-organization-admin-list/lang/cy-gb.js
+++ components/d2l-organization-admin-list/lang/cy-gb.js
@@ -3,15 +3,15 @@
 export default {
 	"actions": "Camau Gweithredu", // Label for learning path context menu
 	"actionsForLP": "Gweithrediadau ar gyfer {name}", // Label for within the context menu
-	"confirmDeleteMessage": "Bydd yr eitem hon yn cael ei symud i\'r bin ailgylchu a gellir ei hadfer neu ei dileu yn barhaol yn nes ymlaen.", // The message for the delete Learning Path confirmation dialog
+	"confirmDeleteMessage": "Bydd yr eitem hon yn cael ei symud i'r bin ailgylchu a gellir ei hadfer neu ei dileu yn barhaol yn nes ymlaen.", // The message for the delete Learning Path confirmation dialog
 	"confirmDeleteTitle": "Cadarnhau Dileu", // The title for the delete Learning Path confirmation dialog
 	"createLearningPath": "Creu Llwybr Dysgu", // Button to create a new Learning Path
 	"defaultLearningPathName": "Llwybr Dysgu Dideitl", // Default name given to a newly created Learning Path
 	"deleteSucceeded": "Tynnwyd {name} yn llwyddiannus.", // The toast message displayed when a Learning Path is successfully deleted
-	"hiddenFromUsers": "Wedi\'i guddio rhag defnyddwyr", // Visibility status displayed when a learning path is not visible
+	"hiddenFromUsers": "Wedi'i guddio rhag defnyddwyr", // Visibility status displayed when a learning path is not visible
 	"noAction": "Canslo", // The 'No' button action text in the delete Learning Path confirmation dialog
 	"noLearningPath": "Does dim llwybrau dysgu i’w harddangos", // A page that lists learning paths this shows up when there are none to list.
-	"noLearningPathWithSearchTerm": "Canfuwyd 0 canlyniad chwilio ar gyfer \"{searchTerm}”", // After searching for learning paths this shows up when there are no search results.
+	"noLearningPathWithSearchTerm": "Canfuwyd 0 canlyniad chwilio ar gyfer \"{searchTerm}\"", // After searching for learning paths this shows up when there are no search results.
 	"pageNext": "I’r dudalen nesaf", // Label for the next page button
 	"pagePrevious": "I’r dudalen flaenorol", // Label for the back to previous page button
 	"pageSelection": "Ar dudalen {pageCurrent} o {pageTotal}. Rhowch rif tudalen i fynd i’r dudalen honno", // Label for the page number input that lists the current page and lets the user jump to specific pages
--- components/d2l-organization-availability-set/lang/cy-gb.js
+++ components/d2l-organization-availability-set/lang/cy-gb.js
@@ -6,7 +6,7 @@ export default {
 	"explicitItemDescription": "Y {type}: {name}", // Label for an explicit orgunit availability with org unit type and name
 	"inheritItemDescription": "Pob Uned Sefydliad o dan y {type}: {name}", // Label for an inherit (all descendants) orgunit availability
 	"inheritItemWithDescendantTypeDescription": "Pob {descendantType} o dan y {type}: {name}", // Label for an inherit with specific {descendantType} orgunit availability
-	"removeAvailabilityFor": "Tynnu\'r argaeledd ar gyfer {itemDescription}", // Alt text for remove availability 'X' button
+	"removeAvailabilityFor": "Tynnu'r argaeledd ar gyfer {itemDescription}", // Alt text for remove availability 'X' button
 	"availabilityRemoved": "Wedi tynnu’r argaeledd ar gyfer {itemDescription}", // Aria announcement when an availability is removed
 	"notAvailableToCurrentOrgUnit": "Nid yw ar gael i’r Uned Sefydliad Bresennol: {name}", // Aria announcement. {name} is current org unit name.
 	"availableToCurrentOrgUnit": "Ar Gael i’r Uned Sefydliad Bresennol: {name}", // Aria announcement. {name} is current org unit name.
