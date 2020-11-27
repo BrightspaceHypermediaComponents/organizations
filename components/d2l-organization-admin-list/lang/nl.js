9fbca4c6886efd943ec3210350e1f3f15a9507e8
JenkinsReader <resupport@desire2learn.com>
2020-11-27 11:39:33 -0500

    Translation Updates

--- components/d2l-organization-admin-list/lang/nl.js
+++ components/d2l-organization-admin-list/lang/nl.js
@@ -1,21 +1,21 @@
 /* eslint quotes: 0 */
 
 export default {
-	"actions": "Actions", // Label for learning path context menu
-	"actionsForLP": "Actions for {name}", // Label for within the context menu
+	"actions": "Acties", // Label for learning path context menu
+	"actionsForLP": "Acties voor {name}", // Label for within the context menu
 	"confirmDeleteMessage": "Dit item wordt verplaatst naar de prullenbak en kan later worden hersteld of permanent worden verwijderd.", // The message for the delete Learning Path confirmation dialog
 	"confirmDeleteTitle": "Verwijderen bevestigen", // The title for the delete Learning Path confirmation dialog
 	"createLearningPath": "Leertraject maken", // Button to create a new Learning Path
 	"defaultLearningPathName": "Naamloos leertraject", // Default name given to a newly created Learning Path
-	"deleteSucceeded": "Verwijderd", // The toast message displayed when a Learning Path is successfully deleted
+	"deleteSucceeded": "{name} is verwijderd.", // The toast message displayed when a Learning Path is successfully deleted
 	"hiddenFromUsers": "Verborgen voor gebruikers", // Visibility status displayed when a learning path is not visible
 	"noAction": "Annuleren", // The 'No' button action text in the delete Learning Path confirmation dialog
 	"noLearningPath": "Er zijn geen leertrajecten om weer te geven", // A page that lists learning paths this shows up when there are none to list.
-	"noLearningPathWithSearchTerm": "0 zoekresultaten gevonden voor \"{searchTerm}\"", // After searching for learning paths this shows up when there are no search results.
+	"noLearningPathWithSearchTerm": "0 zoekresultaten gevonden voor \'{searchTerm}\'", // After searching for learning paths this shows up when there are no search results.
 	"pageNext": "Naar volgende pagina", // Label for the next page button
 	"pagePrevious": "Naar vorige pagina", // Label for the back to previous page button
 	"pageSelection": "Op pagina {pageCurrent} van {pageTotal}. Voer een paginanummer in om naar die pagina te gaan", // Label for the page number input that lists the current page and lets the user jump to specific pages
-	"removeLearningPath": "Delete", // Label for delete learning path button
+	"removeLearningPath": "verwijderen", // Label for delete learning path button
 	"search": "Zoeken", // Label for the search input to search the list of org units
 	"searchPlaceholder": "Zoeken...", // Placeholder text for the search input to search the list of org units
 	"visibleToUsers": "Zichtbaar voor gebruikers", // Visibility status displayed when a learning path is visible
--- components/d2l-organization-completion/lang/ja.js
+++ components/d2l-organization-completion/lang/ja.js
@@ -1,18 +1,20 @@
 /* eslint quotes: 0 */
 
 export default {
-	"cancelText" : "キャンセル", // Button text for cancelling changes
-	"checkboxLabel" : "このコースの終了トラッキングを有効化", // Label for turning on completion tracking checkbox
-	"disableWarning" : "終了トラッキングを無効化すると、受講者の終了データが失われる場合があります。", // Warning text for disabling existing completion tracking
-	"dlgDisableNegativeButtonText" : "キャンセル", // Button label for cancelling disabling completion dialog
-	"dlgDisablePositiveButtonText" : "無効化", // Button label for disabling completion dialog
-	"dlgDisableSecondaryMessage" : "終了トラッキングを無効化すると、受講者の終了データが失われる場合があります。<br/><br/>コース終了トラッキングを無効化してもよろしいですか？", // Warning info for disabling completion dialog
-	"dlgDisableTitle" : "コース終了トラッキングを無効化しますか？", // Title for disable completion dialog
-	"enableWarning" : "受講者がコースの開始後に終了トラッキングを有効化すると、機能を有効化する前に発生したコースの終了は追跡されません。", // Warning for enabling completion
-	"errGeneric" : "申し訳ありません。エラーが発生しました。後でもう一度試してください。", // Generic error text
-	"errNoaccess" : "終了トラッキングを管理するためにアクセスする権限がありません。", // Permissions error text
-	"mainDesc" : "コース終了トラッキングは、目次に含まれるすべての「必須」アクティビティが完了した場合に、受講者を完了としてマークする機能です。", // Description
-	"mainTitle" : "コース終了トラッキング", // Title
-	"saveText" : "保存して閉じる", // Save button text
-	"subTitle" : "終了トラッキング設定" // Subtitle
+	"cancelText": "キャンセル", // Button text for cancelling changes
+	"checkboxLabel": "このコースの終了トラッキングを有効化", // Label for turning on completion tracking checkbox
+	"disableWarning": "終了トラッキングを無効化すると、受講者の終了データが失われる場合があります。", // Warning text for disabling existing completion tracking
+	"displayProgressCheckboxLabel": "進捗率の表示", // Label for turning on progress percentage display
+	"displayProgressCheckboxSubText": "進捗率の表示は、コース終了トラッキングが有効になっている場合にのみ使用できます。", // Help text for progress display checkbox
+	"dlgDisableNegativeButtonText": "キャンセル", // Button label for cancelling disabling completion dialog
+	"dlgDisablePositiveButtonText": "無効化", // Button label for disabling completion dialog
+	"dlgDisableSecondaryMessage": "終了トラッキングを無効化すると、受講者の終了データが失われる場合があります。<br/><br/>コース終了トラッキングを無効化してもよろしいですか？", // Warning info for disabling completion dialog
+	"dlgDisableTitle": "コース終了トラッキングを無効化しますか？", // Title for disable completion dialog
+	"enableWarning": "受講者がコースの開始後に終了トラッキングを有効化すると、機能を有効化する前に発生したコースの終了は追跡されません。", // Warning for enabling completion
+	"errGeneric": "申し訳ありません。エラーが発生しました。後でもう一度試してください。", // Generic error text
+	"errNoaccess": "終了トラッキングを管理するためにアクセスする権限がありません。", // Permissions error text
+	"mainDesc": "コース終了トラッキングは、目次に含まれるすべての「必須」アクティビティが完了した場合に、受講者を完了としてマークする機能です。", // Description
+	"mainTitle": "コース終了トラッキング", // Title
+	"saveText": "保存して閉じる", // Save button text
+	"subTitle": "終了トラッキング設定", // Subtitle
 };
