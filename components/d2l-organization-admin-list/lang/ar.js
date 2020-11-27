58cc051658cdbfafea9175c3b24d5f5f37f2d070
JenkinsReader <resupport@desire2learn.com>
2020-11-27 10:56:16 -0500

    Translation Updates

--- components/d2l-organization-admin-list/lang/ar.js
+++ components/d2l-organization-admin-list/lang/ar.js
@@ -1,23 +1,23 @@
 /* eslint quotes: 0 */
 
 export default {
-	"actions": "Actions", // Label for learning path context menu
-	"actionsForLP": "Actions for {name}", // Label for within the context menu
-	"confirmDeleteMessage": "This item will be moved to the recycle bin and can be restored or permanently deleted later.", // The message for the delete Learning Path confirmation dialog
-	"confirmDeleteTitle": "Confirm Delete", // The title for the delete Learning Path confirmation dialog
+	"actions": "الإجراءات", // Label for learning path context menu
+	"actionsForLP": "الإجراءات لـ {name}", // Label for within the context menu
+	"confirmDeleteMessage": "سيتم نقل هذا العنصر إلى سلة المحذوفات ويمكن استعادته أو حذفه نهائيًا في وقت لاحق.", // The message for the delete Learning Path confirmation dialog
+	"confirmDeleteTitle": "تأكيد الحذف", // The title for the delete Learning Path confirmation dialog
 	"createLearningPath": "إنشاء مسار التعلّم", // Button to create a new Learning Path
 	"defaultLearningPathName": "مسار تعلّم من دون عنوان", // Default name given to a newly created Learning Path
-	"deleteSucceeded": "تم الحذف بنجاح", // The toast message displayed when a Learning Path is successfully deleted
+	"deleteSucceeded": "تمت إزالة {name} بنجاح.", // The toast message displayed when a Learning Path is successfully deleted
 	"hiddenFromUsers": "مخفي عن المستخدمين", // Visibility status displayed when a learning path is not visible
 	"noAction": "إلغاء", // The 'No' button action text in the delete Learning Path confirmation dialog
-	"noLearningPath": "There are no learning paths to display", // A page that lists learning paths this shows up when there are none to list.
-	"noLearningPathWithSearchTerm": "لم يتم العثور على أي نتيجة بحث لـ \"{searchTerm}\"", // After searching for learning paths this shows up when there are no search results.
+	"noLearningPath": "ما من مسارات تعلّم لعرضها", // A page that lists learning paths this shows up when there are none to list.
+	"noLearningPathWithSearchTerm": "تم العثور على 0 نتيجة بحث للمصطلح \"{searchTerm}\"", // After searching for learning paths this shows up when there are no search results.
 	"pageNext": "الانتقال إلى الصفحة التالية", // Label for the next page button
 	"pagePrevious": "الانتقال إلى الصفحة السابقة", // Label for the back to previous page button
 	"pageSelection": "في الصفحة {pageCurrent} من أصل {pageTotal}. إدخال رقم صفحة للانتقال إليها", // Label for the page number input that lists the current page and lets the user jump to specific pages
-	"removeLearningPath": "Delete", // Label for delete learning path button
+	"removeLearningPath": "حذف", // Label for delete learning path button
 	"search": "بحث", // Label for the search input to search the list of org units
 	"searchPlaceholder": "البحث...", // Placeholder text for the search input to search the list of org units
 	"visibleToUsers": "ظاهر للمستخدمين", // Visibility status displayed when a learning path is visible
-	"yesAction": "Delete Learning Path" // The 'Yes' button action text in the delete Learning Path confirmation dialog
+	"yesAction": "حذف مسار التعلّم" // The 'Yes' button action text in the delete Learning Path confirmation dialog
 };
